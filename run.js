f584f1b607d259e0098a34d305980dcb6dd78803
Sam Tebbs <samuel.tebbs@gmail.com>
2017-07-20 17:35:16 +0100

    Fixed more things xDD

--- run.js
+++ run.js
@@ -3,23 +3,26 @@
  */
 const fs = require("fs")
 var elk = require("./elk.js")
+var args = require('minimist')(process.argv.slice(2));
 
-var argStart = 0
+var files = args["_"]
 
-var args = process.argv;
-if(args.length > argStart) {
-    var file = args[argStart];
-    var out = args.length > argStart + 1 ? args[argStart + 1] : "."
-    var data = args.length > argStart + 2 ? JSON.parse(fs.readFileSync(args[4], 'utf8')) : null
-    var compileResult = elk.compileFiles([file], out, data, null)
+var out = args["o"]
+if(!out) out = "."
 
-    // Print error data
-    Object.keys(compileResult).forEach(function (key) {
-        var result = compileResult[key]
-        if(result.errored === true) {
-            console.log(key)
-            console.log(result.errData)
-            console.log("\n")
-        }
-    })
+var dataFile = args["d"]
+var data = {}
+if(dataFile) {
+    data = JSON.parse(fs.readFileSync(dataFile, 'utf8'));
 }
+
+var compileResult = elk.compileFiles(files, out, data)
+// Print error data
+Object.keys(compileResult).forEach(function (key) {
+    var result = compileResult[key]
+    if(result.errored === true) {
+        console.log(key)
+        console.log(result.errData)
+        console.log("\n")
+    }
+})
