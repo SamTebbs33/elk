787ddcf6d7472f340ae3e6bad28e22c12e25c069
Sam Tebbs <samuel.tebbs@gmail.com>
2017-09-21 18:30:26 +0100

    New version with CodeMirror editors

--- js/script.js
+++ js/script.js
@@ -1,45 +1,105 @@
-setInterval(compile, 50)
-var oldText = ""
-var elkCookieName = "elk-text"
-var cookieName = "html-text"
-var errorCookieName = "error-text"
+/** var quillElk = new Quill('#elk-text', {
+  modules: {
+    toolbar: false
+  },
+  theme: 'snow'
+});
 
-function setError(msg) {
-    $("#error-text").html(msg)
-    Cookies.set(errorCookieName, msg)
+var quillJson = new Quill('#json-text', {
+  modules: {
+    toolbar: false
+  },
+  theme: 'snow'
+});
+**/
+
+var oldElkText = ""
+var oldJsonText = ""
+var oldCssText = ""
+var newHtmlText = ""
+var elkError = document.getElementById("error")
+var jsonError = document.getElementById("error")
+var elkTextArea = document.getElementById("elk-text")
+var jsonTextArea = document.getElementById("json-text")
+var cssTextArea = document.getElementById("css-text")
+
+var jsonEditor = CodeMirror(jsonTextArea, {
+  value: '{\n\t"header":  "Hi :)"\n}',
+  mode:  "application/json",
+  lineNumbers: true,
+  styleActiveLine: true,
+  matchBrackets: true
+});
+
+var elkEditor = CodeMirror(elkTextArea, {
+  value: 'html {\n\thead {\n\t\t\n\t}\n\tbody {\n\t\th1: $header\n\t}\n}',
+  mode:  "json",
+  lineNumbers: true,
+  styleActiveLine: true,
+  matchBrackets: true
+});
+
+var cssEditor = CodeMirror(cssTextArea, {
+  value: 'h1 {\n\tcolor: blue;\n}',
+  mode:  "css",
+  lineNumbers: true,
+  styleActiveLine: true,
+  matchBrackets: true
+});
+
+function replace(existing, editor) {
+  editorElement = editor.getWrapperElement()
+  for (var cls of existing.classList) {
+    editorElement.classList.add(cls)
+  }
+  existing.parentNode.replaceChild(editorElement, existing);
 }
 
-function setHTML(msg) {
-    $("#html-text").html(msg)
-    Cookies.set(cookieName, msg)
+replace(cssTextArea, cssEditor)
+replace(jsonTextArea, jsonEditor)
+replace(elkTextArea, elkEditor)
+setInterval(compile, 200)
+
+function error(msg, element) {
+  element.setAttribute("title", msg)
+  element.style.visibility = "visible"
 }
 
-function setIfExists(name, func) {
-    var cookie = Cookies.get(name)
-    if(cookie !== undefined) func(cookie)
+function clearError(element) {
+  element.setAttribute("title", "")
+  element.style.visibility = "hidden"
 }
 
-function setElk(text) {
-    Cookies.set(elkCookieName, text)
+function setHTML(html, style) {
+  document.getElementById("html-text").innerHTML = "<style>" + style + "</style>" + html
 }
-
-setIfExists(cookieName, setHTML)
-setIfExists(elkCookieName, setElk)
-setIfExists(errorCookieName, setError)
 
 function compile() {
-    var elkText = $("#elk-text").val()
-    if(elkText !== oldText) {
-        oldText = elkText
-        var result = elk.compile(elkText, {}, 0)
-        if(!result.errored) {
-            setError("")
-            setHTML(result.data)
-        } else {
-            errData = result.errData;
-            errMsg = typeof errData === 'string' ? errData : "Syntax error at " + errData.location.line + ", " + errData.location.column;
-            setError(errMsg)
-        }
-        console.log(result)
+  var elkText = elkEditor.getValue()
+  var jsonText = jsonEditor.getValue()
+  var styleText = cssEditor.getValue()
+  if(elkText !== oldElkText || jsonText !== oldJsonText) {
+    oldElkText = elkText
+    oldJsonText = jsonText
+    oldCssText = styleText
+    try {
+      var compileResult = elk.compile(elkText, JSON.parse(jsonText))
+      if(!compileResult.errored) {
+        newHtmlText = compileResult.data
+        clearError(jsonError)
+        clearError(elkError)
+        setHTML(newHtmlText, styleText)
+      } else {
+        var errData = compileResult.errData
+        var errMsg = typeof errData === 'string' ? errData : "Syntax error at " + errData.location.line + ", " + errData.location.column;
+        clearError(jsonError)
+        error(errMsg, elkError)
+      }
+    } catch(e) {
+      error(e, jsonError)
     }
+  } else if(styleText !== oldCssText) {
+    oldCssText = styleText
+    setHTML(newHtmlText, styleText)
+  }
 }
